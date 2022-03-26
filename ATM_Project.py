1c56eb4c706550208172c2f72348cae481915f7b
Faiza ahmed <68142873+faiza333@users.noreply.github.com>
2022-03-26 18:56:13 +0200

    Add files via upload

---
+++ ATM_Project.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 30 11:00:48 2021
+
+@author: faiza
+"""
+
+import tkinter as k
+
+main_window = k.Tk()
+main_window.title("ATM")
+entry = tkinter.Entry(main_window, width = 30, fg = "blue", show = "*")
+entry.pack()
